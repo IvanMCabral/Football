b89cff36b39699911de118885bc910f8472a5e2a
ichu_user <ichu_@example.com>
2026-08-03 16:35:01 -0300

    Use dedicated runtime auth failures

--- src/main/java/com/footballmanager/application/exception/AuthCredentialsException.java
+++ src/main/java/com/footballmanager/application/exception/AuthCredentialsException.java
@@ -1,7 +1,7 @@
 package com.footballmanager.application.exception;
 
 /** Safe authentication failure for unknown users or invalid passwords. */
-public class AuthCredentialsException extends IllegalArgumentException {
+public class AuthCredentialsException extends RuntimeException {
     public AuthCredentialsException(String message) {
         super(message);
     }
