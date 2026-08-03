ae8c8c9128fc151b891288315e543d7e6ab175b2
ichu_user <ichu_@example.com>
2026-08-03 15:52:18 -0300

    Harden auth exception classification

--- src/main/java/com/footballmanager/application/exception/AuthValidationException.java
+++ src/main/java/com/footballmanager/application/exception/AuthValidationException.java
@@ -1,7 +1,7 @@
 package com.footballmanager.application.exception;
 
 /** Validation failure for an authentication request. */
-public class AuthValidationException extends IllegalArgumentException {
+public class AuthValidationException extends RuntimeException {
     public AuthValidationException(String message) {
         super(message);
     }
