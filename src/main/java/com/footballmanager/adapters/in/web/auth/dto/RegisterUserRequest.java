71741aeaeca398e95d7655c293c74d61b7c58cd3
ichu_user <ichu_@example.com>
2026-08-09 19:42:11 -0300

    Make public registration payload binding explicit

--- src/main/java/com/footballmanager/adapters/in/web/auth/dto/RegisterUserRequest.java
+++ src/main/java/com/footballmanager/adapters/in/web/auth/dto/RegisterUserRequest.java
@@ -1,7 +1,14 @@
 package com.footballmanager.adapters.in.web.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record RegisterUserRequest(
-    String email,
-    String username,
-    String password
-) {}
+    @JsonProperty("email") String email,
+    @JsonProperty("username") String username,
+    @JsonProperty("password") String password
+) {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public RegisterUserRequest {
+    }
+}
